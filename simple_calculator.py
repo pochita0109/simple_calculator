6008ec1dff5f97182c8516c9ab346bdd77e74dae
pochita0109 <akosicosta@gmail.com>
2023-05-11 16:34:01 -0700

    Added pseudocodes

---
+++ simple_calculator.py
@@ -0,0 +1,25 @@
+# Kenneth John Costa
+# Assignment 5
+# Simple Calculator
+
+# Show the operation that is available on simple calculator
+# Ask the user to enter the first number
+# Ask the user to enter the second number
+# Ask the user about the operation they want to use (1-4)
+    # If the user entered invalid character:
+
+# If addition (1)
+    # Print the result
+# If subtraction (2)
+    # Print the result 
+# If multiplication (3)
+    # Print the result
+# if division (4)
+    # Print the result
+    # If the user entered zero as a divisor:
+
+# Ask the user if they want to continue on using mini calculator (Y/N)
+# If Y:
+# If N:
+# If the user entered other characters than Y or N:
+    
